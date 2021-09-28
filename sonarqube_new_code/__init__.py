55467d93a19244ff42fc6c139d84f18765ab92e7
Hector Espert <hector@mrjeffapp.com>
2021-09-28 12:12:57 +0200

    Add debug option

--- sonarqube_new_code/__init__.py
+++ sonarqube_new_code/__init__.py
@@ -12,10 +12,15 @@ def sonarqube_new_code():
     parser.add_argument('-p', action="store", dest="project", required=True)
     parser.add_argument('-v', action="store", dest="version", default="BASELINE")
     parser.add_argument('-b', action="store", dest="branch", default="master")
+    parser.add_argument('-d', action="store", dest="debug", default=False)
 
     parameters = parser.parse_args()
 
+    print("Getting project analyses")
     r = requests.get("{}/api/project_analyses/search?project={}&category=VERSION&ps=500".format(parameters.url, parameters.project), auth=(parameters.token, ''))
+    if parameters.debug:
+        print("- Response code: {}".format(r.status_code))
+        print("- Response body: " + r.text)
     if r.status_code != 200:
         raise SystemExit(r.text)
 
@@ -25,7 +30,13 @@ def sonarqube_new_code():
 
     last_version_analysis_key = last_version_analyses[0]['key']
 
+    print("Selected version key: " + last_version_analysis_key)
+
+    print("Updating project new code specific analysis")
     response = requests.post("{}/api/new_code_periods/set?project={}&branch={}&type=SPECIFIC_ANALYSIS&value={}".format(parameters.url, parameters.project, parameters.branch, last_version_analysis_key), auth=(parameters.token, ''))
+    if parameters.debug:
+        print("- Response code: {}".format(response.status_code))
+        print("- Response body: " + response.text)
     if response.status_code != 200:
         raise SystemExit(response.text)
 
